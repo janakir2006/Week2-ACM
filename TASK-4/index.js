a314c5dbdcea76e859f6111ce2579c010e04b087
janakir2006 <janakir2006@gmail.com>
2025-07-30 13:49:37 +0530

    Add files via upload

---
+++ TASK-4/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
+const day = String(today.getDate()).padStart(2, '0');
+
+const formattedDate = `${year}-${month}-${day}`;
+
+app.get("/", async(req,res)=>{
+    try{
+        console.log(req.body);
+        const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${formattedDate}`);
+        const result = response.data;
+        res.render("index.ejs",{data:result});
+    }catch(error){
+        console.error("Failed to make this request: ",error.message);
+        res.render("index.ejs",{
+            error:error.message,
+        });
+    }
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+});
+
